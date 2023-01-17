47e28a70eea859e7489d8b787c2350414bac703b
Anusha M <118172784+Mandhva@users.noreply.github.com>
2023-01-17 14:06:10 +0530

    Update streamlit_app.py
    added breakfast menu header and 3 items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 
-streamlit.title('My first streamlit app')
-streamlit.header('App for Snowflake data warehouse badge 2 training')
-streamlit.text('Setting up github account')
-streamlit.text('Setting up streamlit account')
+streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
