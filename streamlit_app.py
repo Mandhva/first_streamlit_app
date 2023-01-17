e3d4b77bb6ccd43c510730f6afb97bf68bc4eb6c
Anusha M <118172784+Mandhva@users.noreply.github.com>
2023-01-17 14:02:47 +0530

    Update streamlit_app.py
    adding test lines

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,5 @@ import streamlit
 
 streamlit.title('My first streamlit app')
 streamlit.header('App for Snowflake data warehouse badge 2 training')
+streamlit.text('Setting up github account')
+streamlit.text('Setting up streamlit account')
