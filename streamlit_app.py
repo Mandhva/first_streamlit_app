66bab0824b6f568bdaa150d10d3a0424bc1dbbe4
Anusha M <118172784+Mandhva@users.noreply.github.com>
2023-01-24 16:45:41 +0530

    Update streamlit_app.py
    adding json file

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,3 +19,7 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
 streamlit.header("Fruityvice Fruit Advice!")
+import requests
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response.json())
